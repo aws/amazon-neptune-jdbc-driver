28198a4c33b7f40deec4a474a8e652e22d10e3b1
my name <valentinab@bitquilltech.com>
2021-03-10 19:37:28 -0600

    AN-434 Implemented TACO connection dialog - CR changes.

--- src/TableauConnector/connectionBuilder.js
+++ src/TableauConnector/connectionBuilder.js
@@ -1,5 +1,11 @@
 (function dsbuilder(attr){
     var urlBuilder = "jdbc:neptune:opencypher://bolt://" + attr["server"] + ":" + attr["port"];
+
+    var additionalOptions = attr[connectionHelper.attributeVendor1];
+    if (additionalOptions) {
+        urlBuilder += ";" + additionalOptions;
+    }
+
     logging.log("urlBuilder=" + urlBuilder);
     return [urlBuilder];
 })
--- src/TableauConnector/connectionProperties.js
+++ src/TableauConnector/connectionProperties.js
@@ -5,13 +5,9 @@
     // This script is only needed if you are using a JDBC driver.
     var params = {};
 
-    //set keys for properties needed for connecting using JDBC
+    // Set keys for properties needed for connecting using JDBC
     var AUTH_SCHEME_KEY = "AuthScheme";
     var USE_ENCRYPTION_KEY = "UseEncryption";
-    var LOG_LEVEL_KEY = "LogLevel";
-    var CONNECTION_TIMEOUT_MILLIS_KEY = "ConnectionTimeout";
-    var CONNECTION_RETRY_COUNT_KEY = "ConnectionRetryCount";
-    var CONNECTION_POOL_SIZE_KEY = "ConnectionPoolSize";
 
     var authAttrValue = attr[connectionHelper.attributeAuthentication];
     if (authAttrValue == "NONE")
@@ -25,13 +21,6 @@
     } else {
         params[USE_ENCRYPTION_KEY] = "false";
     }
-
-    params[LOG_LEVEL_KEY] = attr[connectionHelper.attributeVendor1];
-
-    //Set hardcoded default values.
-    params[CONNECTION_TIMEOUT_MILLIS_KEY] = "5000";
-    params[CONNECTION_RETRY_COUNT_KEY] = "1";
-    params[CONNECTION_POOL_SIZE_KEY] = "1000";
 
     // Format the attributes as 'key=value'. By default some values are escaped or wrapped in curly braces to follow the JDBC standard, but you can also do it here if needed.
     var formattedParams = [];
