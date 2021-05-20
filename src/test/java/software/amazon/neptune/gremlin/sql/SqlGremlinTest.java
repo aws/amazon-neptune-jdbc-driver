e1e5a731be31e24d6a9b61a8353cf47cbee70ea0
Lyndon Bauto <lyndonb@bitquilltech.com>
2021-05-20 11:23:25 -0700

    [1] Removing extra line.

--- src/test/java/software/amazon/neptune/gremlin/sql/SqlGremlinTest.java
+++ src/test/java/software/amazon/neptune/gremlin/sql/SqlGremlinTest.java
@@ -133,8 +133,7 @@ public class SqlGremlinTest {
         }
         return stringRows;
     }
-
-
+    
     void printVertexes() {
         System.out.println("Input vertexes:");
         System.out.println("\t\tPerson: {\n" +
